15f9cd7bed844bf285d13a345ad175c0be9d58ad
maulik_parmar_mentora <maulik@mentora.institute>
2022-09-03 22:42:21 +0530

    Added New Test Cases For Button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Button Has Correct Initial Color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Find an element with a role of button and text of 'Change To Blue'
+  const colorButton = screen.getByRole('button', { name: /Change To Blue/i });
+
+  // Expect the background color to be red
+  expect(colorButton).toHaveStyle({ background: 'red' });
+
+});
+
+test('Button Has Correct Initial Text', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: /Change To Blue/i });
+
+  // Click The Button
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ background: 'blue' });
+
+  // Expect The Button Text To Be 'Change To Red'
+  expect(colorButton.textContent).toBe('Change To red');
 });
